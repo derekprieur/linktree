5b1e66b1b370d1fcb0ffa2e4a84e8d34f51b658a
derekprieur <DPrieur123@gmail.com>
2023-01-09 10:39:58 -0500

    initial commit

---
+++ data.js
@@ -0,0 +1,56 @@
+import { FiGithub, FiInstagram, FiTwitter } from "react-icons/fi";
+import { AiOutlineGithub, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineMail, AiOutlineTwitter, AiOutlineYoutube } from "react-icons/ai";
+import { IoLogoTiktok } from "react-icons/io5";
+
+
+export const data = {
+    "name": "Derek Prieur",
+    "avatar": "https://pbs.twimg.com/profile_images/1602395494000492547/tBma79J2_400x400.jpg",
+    "links": [
+        {
+            "name": "The Vegan Viewpoint",
+            "href": "https://theveganviewpoint.com/"
+        },
+        {
+            "name": "Web Development Portfolio",
+            "href": "https://www.derekprieur.dev/"
+        }
+    ],
+    "socials": [
+        {
+            "name": "Instagram",
+            "href": "https://www.instagram.com/derekprieur/",
+            "icon": <AiOutlineInstagram />
+        },
+        {
+            "name": "YouTube",
+            "href": "https://www.youtube.com/channel/UCZQY9ZQZ9ZQZ9ZQZ9ZQZ9ZQ",
+            "icon": <AiOutlineYoutube />
+        },
+        {
+            "name": "Twitter",
+            "href": "https://twitter.com/derekprieur",
+            "icon": <AiOutlineTwitter />
+        },
+        {
+            "name": "TikTok",
+            "href": "https://www.tiktok.com/@derekprieur",
+            "icon": <IoLogoTiktok />
+        },
+        {
+            "name": "LinkedIn",
+            "href": "https://www.linkedin.com/in/derekprieur/",
+            "icon": <AiOutlineLinkedin />
+        },
+        {
+            "name": "GitHub",
+            "href": "https://github.com/derekprieur",
+            "icon": <AiOutlineGithub />
+        },
+        {
+            "name": "Email",
+            "href": "mailto:dprieur123@gmail.com",
+            "icon": <AiOutlineMail />
+        }
+    ]
+}
